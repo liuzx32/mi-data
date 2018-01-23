f138835a21477a285fb7229617a030468d78409e
liuzhixin <liuzhixin@xiaomi.com>
2018-01-23 16:38:16 +0800

    adjust mail logic

--- src/main/java/mi/data/join/MyForkJoinTest.java
+++ src/main/java/mi/data/join/MyForkJoinTest.java
@@ -6,8 +6,9 @@ import java.util.concurrent.Future;
 public class MyForkJoinTest {
 
     public static void main(String[] args) throws Exception {
-        MyForkJoinTask task = new MyForkJoinTask(1, 4);
         ForkJoinPool pool = new ForkJoinPool();
+        //
+        MyForkJoinTask task = new MyForkJoinTask(1, 4);
         Future<Integer> result = pool.submit(task);
         System.out.println("result is " + result.get());
     }
